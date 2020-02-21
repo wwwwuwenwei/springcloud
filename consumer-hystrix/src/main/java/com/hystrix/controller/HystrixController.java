bef6aa629e644fa9e7072256591f5818d6bd9b14
wwwwuwenwei <www3wwuwenwei@163.com>
2020-02-21 14:21:21 +0800

    SpringCloud(Eureka\Ribbon\Feign\Hystrix)

--- consumer-hystrix/src/main/java/com/hystrix/controller/HystrixController.java
+++ consumer-hystrix/src/main/java/com/hystrix/controller/HystrixController.java
@@ -1,9 +1,10 @@
-package com.springcloud.controller;
+package com.hystrix.controller;
 
-
+import com.hystrix.pojo.User;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
-import com.springcloud.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,27 +13,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.naming.Name;
+import java.util.Date;
 
 @RestController
-public class OrderController {
+public class HystrixController {
+
     @Autowired
     RestTemplate restTemplate;//spring 提供的一个用于访问 Rest 接口的模板对象
-//    String url = "http://localhost:7090/user/";
 
-    @Value("${user.url}")
-    private String url;
+    @Qualifier("erekaClient")
+    private EurekaClient erekaClient;
 
-    @Qualifier("orderEurekaClient")
-    private EurekaClient orderEurekaClient;
-
+    @HystrixCommand(fallbackMethod = "shibai", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.strategy",value = "SEMAPHORE")
+    })//"execution.isolation.strategy",默认不建议修改这个值,如果遇到问题再修改,否则不建议
     @GetMapping("/order/{id}")
     public User getOrder(@PathVariable Long id){
         //访问提供者，获取数据
-        InstanceInfo instanceInfo = orderEurekaClient.getNextServerFromEureka("provide-user", false);
+        InstanceInfo instanceInfo = erekaClient.getNextServerFromEureka("provide-user", false);
         String homePageUrl = instanceInfo.getHomePageUrl();
         User user = restTemplate.getForObject(homePageUrl+"/user/"+id, User.class);
         //通过访问 Rest 获取 Json 数据，然后转换为 User 对象
         return user;
     }
+
+    public User shibai(Long id){
+        User user = new User();
+        user.setId(-100);
+        user.setDate(new Date());
+        return user;
+    }
+
 }
